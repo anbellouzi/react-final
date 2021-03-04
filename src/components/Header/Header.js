db7fba44ebe5c8a78b9f6e461420e429b9a66f44
Anas Bellouzi <anbellouzi@gmail.com>
2021-03-03 23:29:36 -0500

    challenge 2 comlete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+// src/Header.js
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1>FEW 1.2 Final Assessment</h1>
+      <h2>Anas Bellouzi</h2>
+    </div>
+  )
+}
+
+export default Header
